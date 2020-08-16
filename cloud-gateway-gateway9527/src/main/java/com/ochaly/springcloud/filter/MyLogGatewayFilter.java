b710752b0ae4133281ef364f9d1aa3a6e0839f21
ouchonghui <ouchonghui@csii.com.cn>
2020-08-16 09:48:00 +0800

    添加filters

---
+++ cloud-gateway-gateway9527/src/main/java/com/ochaly/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,38 @@
+package com.ochaly.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @Author: ouchonghui
+ * @Description:
+ * @Date: create in 2020/8/16 09:41
+ */
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("*********come in MyLogGatewayFilter: " + new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null) {
+            log.info("*********用户名为空，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
