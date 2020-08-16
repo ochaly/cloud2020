24718b74f292372421e5eeaf2b649b2f971937a2
ouchonghui <ouchonghui@csii.com.cn>
2020-08-16 09:29:42 +0800

    添加各种断言规则

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Author: ouchonghui
+ * @Description:
+ * @Date: create in 2020/8/16 09:15
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
